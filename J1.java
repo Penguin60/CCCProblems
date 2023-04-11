26a61f0136abe05e4198a985a8707b9531463923
Allan <allanye2009@gmail.com>
2023-04-11 12:07:07 -0400

    Add J1

--- J1.java
+++ J1.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class J1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int one = sc.nextInt();
+        int two = sc.nextInt();
+        int three = sc.nextInt();
+
+        int score = one + two * 2 + three * 3;
+
+        if(score >= 10){
+            System.out.println("happy");
+        } else {
+            System.out.println("sad");
+        }
+    }
 }
