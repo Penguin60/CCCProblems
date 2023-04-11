cf5f67703c353c3c65076301ec51132ae8d9013c
Dean <radean.rashed@gmail.com>
2023-04-11 11:59:12 -0400

    Add J2

--- J2.java
+++ J2.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class J2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+        int N = sc.nextInt();
+        int R = sc.nextInt();
+        int sum = N;
+        int i = 0;
+        while(sum <= P) {
+            i++;
+            sum += N*R;
+            N *= R;
+        }
+        System.out.println(i);
+    }
+
+
 }
